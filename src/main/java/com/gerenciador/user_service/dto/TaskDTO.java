bc9eb7b52c5b92231b9c424e21849b0400daa40b
Jaiany <jaianyoliveira031@gmail.com>
2025-02-25 14:54:46 -0300

    add task dto and builders

---
+++ src/main/java/com/gerenciador/user_service/dto/TaskDTO.java
@@ -0,0 +1,74 @@
+package com.gerenciador.user_service.dto;
+
+import java.time.LocalDateTime;
+
+public class TaskDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private String priority;
+    private LocalDateTime dueDate;
+
+    // Construtor
+    public TaskDTO() {
+    }
+
+    // Construtor com todos os campos
+    public TaskDTO(Long id, String title, String description, String status, String priority, LocalDateTime dueDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+        this.dueDate = dueDate;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
