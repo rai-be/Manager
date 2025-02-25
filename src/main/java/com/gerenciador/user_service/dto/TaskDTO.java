ed4ce362b607f09bb2a5eb56b8f03b552ee136bc
Jaiany <jaianyoliveira031@gmail.com>
2025-02-25 14:57:18 -0300

    Add Task Dto using Lombok

--- src/main/java/com/gerenciador/user_service/dto/TaskDTO.java
+++ src/main/java/com/gerenciador/user_service/dto/TaskDTO.java
@@ -1,7 +1,14 @@
 package com.gerenciador.user_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskDTO {
     private Long id;
     private String title;
@@ -10,65 +17,5 @@ public class TaskDTO {
     private String priority;
     private LocalDateTime dueDate;
 
-    // Construtor
-    public TaskDTO() {
-    }
 
-    // Construtor com todos os campos
-    public TaskDTO(Long id, String title, String description, String status, String priority, LocalDateTime dueDate) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.status = status;
-        this.priority = priority;
-        this.dueDate = dueDate;
-    }
-
-    // Getters e Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getPriority() {
-        return priority;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
-
-    public LocalDateTime getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(LocalDateTime dueDate) {
-        this.dueDate = dueDate;
-    }
+}
