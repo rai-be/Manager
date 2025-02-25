3e03465a9f5ee4417f88c0b8eaf927a478610542
Jaiany <jaianyoliveira031@gmail.com>
2025-02-25 13:59:46 -0300

    add model task and getters and setters

---
+++ src/main/java/com/gerenciador/user_service/model/Task.java
@@ -0,0 +1,91 @@
+package com.gerenciador.user_service.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String category;
+    private String priority;
+    private boolean completed;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // Construtor padrão
+    public Task() {}
+
+    // Construtor com id
+    public Task(Long id) {
+        this.id = id;
+    }
+
+    // Getters e setters manualmente
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
