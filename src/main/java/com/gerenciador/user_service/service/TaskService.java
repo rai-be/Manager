d59a765edb98b66e2cabff6fa5715f2498c0bf9c
Jaiany <jaianyoliveira031@gmail.com>
2025-02-25 14:42:57 -0300

    add list and search

--- src/main/java/com/gerenciador/user_service/service/TaskService.java
+++ src/main/java/com/gerenciador/user_service/service/TaskService.java
@@ -1,5 +1,6 @@
 package com.gerenciador.user_service.service;
 
+import com.gerenciador.user_service.model.Task;
 import com.gerenciador.user_service.respository.TaskRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -17,4 +18,15 @@ public class TaskService {
     public TaskService(TaskRepository taskRepository, NotificationClient notificationClient) {
         this.taskRepository = taskRepository;
         this.notificationClient = notificationClient;
+    }
+
+    //lista de tarefas
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    //busca por id
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found")); //se nao existir mostra exceção
     }
