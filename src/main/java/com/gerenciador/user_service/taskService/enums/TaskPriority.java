59909d90ec02724e8cbab149d9d24fbf1a2107e2
Jaiany <jaianyoliveira031@gmail.com>
2025-02-25 21:13:25 -0300

    Add priority enum

---
+++ src/main/java/com/gerenciador/user_service/taskService/enums/TaskPriority.java
@@ -0,0 +1,8 @@
+package com.gerenciador.user_service.taskService.enums;
+
+public enum TaskPriority {
+    LOW,       // Baixa prioridade
+    MEDIUM,    // Média prioridade
+    HIGH,      // Alta prioridade
+    CRITICAL   // Urgente
+}
