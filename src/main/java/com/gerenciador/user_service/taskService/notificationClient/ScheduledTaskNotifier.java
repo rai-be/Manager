c24d708b78c51cd3ca8f26605bfcdc1f7296bb42
Jaiany <jaianyoliveira031@gmail.com>
2025-02-25 21:30:53 -0300

    add markup task notifier

--- src/main/java/com/gerenciador/user_service/taskService/notificationClient/ScheduledTaskNotifier.java
+++ src/main/java/com/gerenciador/user_service/taskService/notificationClient/ScheduledTaskNotifier.java
@@ -1,6 +1,14 @@
 package com.gerenciador.user_service.taskService.notificationClient;
 
+import com.gerenciador.user_service.taskService.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ScheduledTaskNotifier {
 
+    private final TaskRepository taskRepository;
+    private final NotificationClient notificationClient;
 
 }
